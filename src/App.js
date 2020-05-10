a44ca1c033c871b1dab920aa3d229e04821a5281
Mussarath Sheriff <mussarathsheriff@Mussaraths-MacBook-Air.local>
2020-05-10 19:12:09 +0530

    deploy react app intial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './sourceImages/r88088.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App Deploy in GitHub</h1>
+      <div><label>Aurthor:</label>Mussarath Sheriff</div>
+      <img className="img-responsive img-thumbnail" src={profileImage} alt="profile" />
     </div>
   );
 }
