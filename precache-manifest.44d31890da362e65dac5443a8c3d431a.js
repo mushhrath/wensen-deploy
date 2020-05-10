93bb475d505b077adadcf1549c368f4138a05de8
Mussarath Sheriff <mussarathsheriff@Mussaraths-MacBook-Air.local>
2020-05-10 19:13:35 +0530

    Updates

---
+++ precache-manifest.44d31890da362e65dac5443a8c3d431a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9dc5e296b4a6df464da6a3947d7dce",
+    "url": "/wensen-deploy/index.html"
+  },
+  {
+    "revision": "003789e542ef082b9dfb",
+    "url": "/wensen-deploy/static/css/main.b126ed20.chunk.css"
+  },
+  {
+    "revision": "ca832bab7ec5e4f2170c",
+    "url": "/wensen-deploy/static/js/2.153038fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wensen-deploy/static/js/2.153038fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003789e542ef082b9dfb",
+    "url": "/wensen-deploy/static/js/main.2cc07ce2.chunk.js"
+  },
+  {
+    "revision": "3b966949cb10f5b72ddd",
+    "url": "/wensen-deploy/static/js/runtime-main.0e8e96f6.js"
+  },
+  {
+    "revision": "7939860c75e2055e879f02f2f8c7a0f1",
+    "url": "/wensen-deploy/static/media/r88088.7939860c.jpg"
+  }
+]);
